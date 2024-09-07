3d99080b325edb03ebe7fc7f2afcba1611c25e24
Flávio Alencar <f.s.alencar1@gmail.com>
2024-09-06 22:23:53 -0300

    Add files via upload

--- Capítulo04/Exemplo03/ClassAverage.java
+++ Capítulo04/Exemplo03/ClassAverage.java
@@ -14,14 +14,14 @@ public class ClassAverage
         //Declaração de Variáveis Locais
         //-----------------------------------------------------------------------------------
         Scanner scan = new Scanner(System.in);
-        double total = 0.0;          //Variável Local que Armazena o Total da Soma das Notas;
-        double nota = 0.0;           //Variável Local que Armazena o valor da Nota; 
+        int total = 0;          //Variável Local que Armazena o Total da Soma das Notas;
+        int nota = 0;           //Variável Local que Armazena o valor da Nota; 
         double average = 0.0;        //Variável Local que Aramazena a Média.
         int gradeCounter = 0;        //Variável Local que Armazena a Quantidade de Notas.
         //-----------------------------------------------------------------------------------
         
         System.out.print("Digite a Nota ou -1 para Parar: ");
-        nota = scan.nextDouble();
+        nota = scan.nextInt();
         System.out.println();
         
         while (nota != -1)
@@ -29,13 +29,13 @@ public class ClassAverage
             total = total + nota;
             gradeCounter = gradeCounter + 1;
             System.out.print("Digite a Nota ou -1 para Parar: ");
-            nota = scan.nextDouble();
+            nota = scan.nextInt();
             System.out.println();
         }
         
         if (gradeCounter != 0)
         {
-            average = total/gradeCounter;
+            average = (double) total/gradeCounter;
             
             System.out.printf("Quantidade de Alunos: %d%n", gradeCounter);
             System.out.printf("Média de Notas da Classe: %.2f%n", average);
