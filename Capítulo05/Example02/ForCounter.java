03134e162e7540e95239357960350dac30e777ed
Flávio Alencar <f.s.alencar1@gmail.com>
2024-09-18 10:55:54 -0300

    Add files via upload

---
+++ Capítulo05/Example02/ForCounter.java
@@ -0,0 +1,19 @@
+//Exemple 02 - Chapter 05
+//Figura 5.2
+//Method: for 
+//Author: Deitel
+//Date of Edition: 18/SET24 - 10h15 AM
+//Flávio Alencar
+
+
+public class ForCounter {
+
+    public static void main(String[] args) 
+    {
+        for (int counter = 1; counter <= 10; counter++)
+        {
+            System.out.printf("%d%s", counter, " ");
+        }
+        
+    }
+}
